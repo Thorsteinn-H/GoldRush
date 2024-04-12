05d833635742ef0c2165bad85c82f98f4adfa3d5
Thorsteinn-H <thorsteinnhilmir@gmail.com>
2024-04-12 09:48:22 +0000

    lagaði test cases fyrir LeikbordTest.java

--- src/test/java/vidmot/LeikbordTest.java
+++ src/test/java/vidmot/LeikbordTest.java
@@ -4,41 +4,51 @@ import javafx.scene.input.KeyCode;
 import static org.junit.Assert.*;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class LeikbordTest {
 
+    private Leikbord leikbord;
+
+    @Before
+    public void setUp() {
+        leikbord = new Leikbord();
+    }
+
+
+    //Býr til gull og kíkir hvort það er á borðinu
     @Test
     public void testFramleidaGull() {
-        Leikbord leikbord = new Leikbord();
         leikbord.framleidaGull();
-        assertEquals(1, leikbord.getChildren().size());
-        assertFalse(leikbord.getGullListi().isEmpty());
+        assertEquals(1, leikbord.getChildren().size()-1); //-1 þarf að vera því grafarinn er í leikborði
+        assertFalse(leikbord.getGullListi().isEmpty()); // Og kíkir hvort listinn er EKKI tómur
     }
 
-    @Test
-    public void testAfram() {
-        Leikbord leikbord = new Leikbord();
-        leikbord.framleidaGull();
-        double initialLayoutX = leikbord.getFxGrafari().getLayoutX();
-        double initialLayoutY = leikbord.getFxGrafari().getLayoutY();
-        leikbord.afram(KeyCode.RIGHT);
-        assertNotEquals(initialLayoutX, leikbord.getFxGrafari().getLayoutX());
-        assertEquals(initialLayoutY, leikbord.getFxGrafari().getLayoutY(), 0);
-    }
 
+    //býr til gull og færir grafaran á eitt gullið og kíkir hvort það hverfur þar sem
+    // þegar grafarinn fer á gull þá hverfur það og nýtt er búið til
     @Test
     public void testErGrefurGull() {
-        Leikbord leikbord = new Leikbord();
         leikbord.framleidaGull();
-        double grafariX = leikbord.getFxGrafari().getLayoutX();
-        double grafariY = leikbord.getFxGrafari().getLayoutY();
-        Gull gull = leikbord.getGullListi().get(0);
-        gull.setLayoutX(grafariX);
-        gull.setLayoutY(grafariY);
 
-        assertEquals(0, leikbord.getChildren().size());
-        assertEquals(0, leikbord.getGullListi().size());
+        // nær í staðsetnigu gullsins
+        double initialGullX = leikbord.getGullListi().get(0).getLayoutX();
+        double initialGullY = leikbord.getGullListi().get(0).getLayoutY();
+
+        // Færir grafaran á gullið
+        leikbord.getFxGrafari().setLayoutX(initialGullX);
+        leikbord.getFxGrafari().setLayoutY(initialGullY);
+        //Þarf að hreyfa til hægri til að virka grefurgull method
+        leikbord.afram(KeyCode.DOWN);
+
+        // nær í staðsetnigu nýja gullsins
+        double newGullX = leikbord.getGullListi().get(0).getLayoutX();
+        double newGullY = leikbord.getGullListi().get(0).getLayoutY();
+
+        // kíkir hvort það hvort nýtt gull var búið til
+        assertNotEquals(initialGullX, newGullX);
+        assertNotEquals(initialGullY, newGullY);
     }
 }
