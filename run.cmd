aa194ba59a6bc23136dfdd87dfed413e968ee958
Thorsteinn-H <thorsteinnhilmir@gmail.com>
2024-04-19 09:34:44 +0000

    bæta jar og run án IDE

---
+++ run.cmd
@@ -0,0 +1,1 @@
+java -jar target\GoldRush-1.0-SNAPSHOT-jar-with-dependencies.jar
---
+++ src/main/java/vidmot/Main.java
@@ -0,0 +1,13 @@
+package vidmot;
+
+/**
+ * Nafn : Þorsteinn H. Erlendsson
+ * Tölvupóstur: the85@hi.is
+ * Lýsing:
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        GoldApplication.main(args);
+    }
+}
